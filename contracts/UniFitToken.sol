7c52e2ccbadb92e6251a97cef65c669cd1450183
bighappyface <anomalous20@gmail.com>
2021-07-26 23:29:29 -0400

    Implement basic 1% deflation with test

--- contracts/UniFitToken.sol
+++ contracts/UniFitToken.sol
@@ -2,9 +2,80 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract UniFitToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("UniFit Token", "UNIFT") {
-        _mint(msg.sender, initialSupply);
-    }
+contract UniFitToken is ERC20, ERC20Burnable {
+  using SafeMath for uint256;
+
+  constructor(uint256 initialSupply) ERC20("UniFit Token", "UNIFT") {
+      _mint(msg.sender, initialSupply);
+  }
+
+  // Institute a minimum supply to prevent over-deflation.
+  uint256 private _minimumSupply = 2000 * (10**18);
+
+  /**
+    * @dev See {ERC20-transfer}.
+    *
+    * Implements transfer with burn.
+    */
+  function transfer(address to, uint256 amount)
+      public
+      override
+      returns (bool)
+  {
+      return super.transfer(to, _partialBurn(amount));
+  }
+
+  /**
+    * @dev See {ERC20-transferFrom}.
+    *
+    * Implements transfer with burn.
+    */
+  function transferFrom(
+      address from,
+      address to,
+      uint256 amount
+  ) public override returns (bool) {
+      return super.transferFrom(from, to, _partialBurn(amount));
+  }
+
+  /**
+    * @dev Handle the burn.
+    *
+    * Used to handle burn activities and return a burned amount.
+    */
+  function _partialBurn(uint256 amount) internal returns (uint256) {
+      uint256 burnAmount = _calculateBurnAmount(amount);
+
+      if (burnAmount > 0) {
+          _burn(msg.sender, burnAmount);
+      }
+
+      return amount.sub(burnAmount);
+  }
+
+  /**
+    * @dev Calculate the burn.
+    *
+    * Evaluate transfer amount to determine desired burn.
+    */
+  function _calculateBurnAmount(uint256 amount)
+      internal
+      view
+      returns (uint256)
+  {
+      uint256 burnAmount = 0;
+
+      if (totalSupply() > _minimumSupply) {
+          burnAmount = amount.div(100);
+          uint256 availableBurn = totalSupply().sub(_minimumSupply);
+          if (burnAmount > availableBurn) {
+              burnAmount = availableBurn;
+          }
+      }
+
+      return burnAmount;
+  }
 }
--- test/UniFitToken.test.js
+++ test/UniFitToken.test.js
@@ -1,21 +1,39 @@
 const { expect } = require("chai");
 
 describe("UniFitToken", function () {
-  it("should mint X number of tokens upon deploy and assign to sender/owner", async function () {
+  // Total Supply
+  const totalSupply = ethers.BigNumber.from("50000000000000000000000000000");
 
+  // Declare consts
+  let owner, secondAddress, UniFitTokenContract, UniFitToken;
+
+  before(async function () {
     // Get signers of local chain
-    const [owner] = await ethers.getSigners();
-
-    // Total Supply
-    const totalSupply = ethers.BigNumber.from("50000000000000000000000000000");
-
+    [owner, secondAddress] = await ethers.getSigners();
     // Deploy contract instance
-    const UniFitTokenContract = await ethers.getContractFactory("UniFitToken");
-    const UniFitToken = await UniFitTokenContract.deploy(totalSupply);
+    UniFitTokenContract = await ethers.getContractFactory("UniFitToken");
+    UniFitToken = await UniFitTokenContract.deploy(totalSupply);
     await UniFitToken.deployed();
+  });
 
+  it("should mint X number of tokens upon deploy and assign to sender/owner", async function () {
     // Assertions
     expect(await UniFitToken.totalSupply()).to.equal(totalSupply);
     expect(await UniFitToken.balanceOf(owner.address)).to.equal(totalSupply);
+  });
+
+  it("should burn some of the token for each transfer", async function () {
+    // Transfer some of the tokens
+    const transferAmount = ethers.BigNumber.from("10000000");
+    await UniFitToken.transfer(secondAddress.address, transferAmount);
+    // Calculate expected amounts amount
+    const burnAmount = transferAmount.div(100);
+    const expectedTotalSupply = totalSupply.sub(burnAmount);
+    const expectedSenderBalance = totalSupply.sub(transferAmount);
+    const expectedReceipientBalance = transferAmount.sub(burnAmount);
+    // Assertions
+    expect(await UniFitToken.totalSupply()).to.equal(expectedTotalSupply);
+    expect(await UniFitToken.balanceOf(owner.address)).to.equal(expectedSenderBalance);
+    expect(await UniFitToken.balanceOf(secondAddress.address)).to.equal(expectedReceipientBalance);
   });
 });
