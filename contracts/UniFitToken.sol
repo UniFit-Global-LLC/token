606aff662cf16f35c48e8c26b5d973760b898137
bighappyface <anomalous20@gmail.com>
2021-07-26 22:45:59 -0400

    Add OpenZepplin and refactor token to ERC20

--- contracts/UniFitToken.sol
+++ contracts/UniFitToken.sol
@@ -1,54 +1,10 @@
 //SPDX-License-Identifier: NOLICENSE
 pragma solidity ^0.8.2;
 
-contract UniFitToken {
-    mapping(address => uint256) public balances;
-    mapping(address => mapping(address => uint256)) public allowance;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    uint256 public totalSupply = 50000000000 * 10**18;
-    string public name = "UniFit Token";
-    string public symbol = "UNIFT";
-    uint256 public decimals = 18;
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
-
-    constructor() {
-        balances[msg.sender] = totalSupply;
-    }
-
-    function balanceOf(address owner) public view returns (uint256) {
-        return balances[owner];
-    }
-
-    function transfer(address to, uint256 value) public returns (bool) {
-        require(balanceOf(msg.sender) >= value, "balance too low");
-        balances[to] += value;
-        balances[msg.sender] -= value;
-        emit Transfer(msg.sender, to, value);
-        return true;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 value
-    ) public returns (bool) {
-        require(balanceOf(from) >= value, "balance too low");
-        require(allowance[from][msg.sender] >= value, "allowance too low");
-        balances[to] += value;
-        balances[from] -= value;
-        emit Transfer(from, to, value);
-        return true;
-    }
-
-    function approve(address spender, uint256 value) public returns (bool) {
-        allowance[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
-        return true;
+contract UniFitToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("UniFit Token", "UNIFT") {
+        _mint(msg.sender, initialSupply);
     }
 }
